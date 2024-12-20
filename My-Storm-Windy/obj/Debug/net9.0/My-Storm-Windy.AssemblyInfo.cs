a2e219604f3dbc07fa11b6775e9039065515ebf7
Sakdar Sukkwan <sakdar.user.hcc@gmail.com>
2024-12-20 10:29:59 +0700

    main

--- My-Storm-Windy/obj/Debug/net9.0/My-Storm-Windy.AssemblyInfo.cs
+++ My-Storm-Windy/obj/Debug/net9.0/My-Storm-Windy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My-Storm-Windy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc7b008d1db8c259863381e8310b7e1f30c37e4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee066b5b701e440e7ae263abeafb471786003fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("My-Storm-Windy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My-Storm-Windy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
